faed16233bb12b02abf82d456b022b2f5427e657
Natasha <n.chalganova@mail.ru>
2022-11-27 16:40:03 +0300

    Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,6 +1,14 @@
-﻿Console.WriteLine("Введите цифру от 1 до 7 (обозначающую день недели)");
-int n = Convert.ToInt32 (Console.ReadLine ());
-if (n == 6 && n == 7)
-Console.WriteLine ("Да");
-else
-Console.WriteLine ("Нет");
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int day) 
+{
+  if (day == 6 || day == 7) 
+  {
+  Console.WriteLine("да, это выходной");
+  }
+  else Console.WriteLine("нет, это будний день");
+}
+
+
+CheckingTheDayOfTheWeek(day);
