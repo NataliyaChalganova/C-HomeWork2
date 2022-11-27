b6a82f4ff402b7ab13f7da6a0341a9a043180964
Natasha <n.chalganova@mail.ru>
2022-11-27 16:26:15 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine ("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string a = Convert.ToString(n);
+if (a.Length > 2)
+{
+  Console.WriteLine ("третья цифра -" + a [2]);
+}
+else
+{
+  Console.WriteLine ("третьей цифры нет");
+}
+
+
+
+
+
+
+
+
+
+
+
+
